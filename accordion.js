e2e83d0fd45655348cd8efcb118b4a6a956632f6
Tremaine T. <128943242+TremMT@users.noreply.github.com>
2025-02-21 16:21:10 -0800

    Updated HTML, CSS, and JS - Accordion works... but still needs work on padding/ margin

--- accordion.js
+++ accordion.js
@@ -1,8 +1,25 @@
-var accordion = document.querySelector('.accordion');
-var panels = accordion.querySelectorAll('.panel');
-var buttons = accordion.querySelectorAll('.accordion-button');
+document.addEventListener("DOMContentLoaded", () => {
+  const faqs = document.querySelectorAll(".faq-container .question, .first-question");
 
+  faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+      const answer = faq.nextElementSibling;
+      const icon = faq.querySelector("img");
 
-
-
+      if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+        answer.style.paddingBottom = "0";
+        icon.src = "assets/images/icon-plus.svg";
+        icon.classList.remove("minus");
+        icon.classList.add("plus");
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+        answer.style.paddingBottom = "20px";
+        icon.src = "assets/images/icon-minus.svg";
+        icon.classList.remove("plus");
+        icon.classList.add("minus");
+      }
+    });
+  });
+});
 
